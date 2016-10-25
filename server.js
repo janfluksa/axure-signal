4f494964e652d1c9c56a18abb9edb29ffc5c87a9
Jan Fluksa <Jan Fluksa>
2016-10-26 01:23:14 +0200

    emit by http post

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
@@ -12,49 +13,60 @@ const app = express();
 const server = app.listen(PORT, () => {   
   console.log('\x1b[36m' + ' Axure signal' + '\x1b[0m \n ------------\n listening on http://localhost:' + PORT + '\n');
 });
-
-app.use(express.static('static'));
+ 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use( express.static('static') ); 
 
 const io = socketIO(server);
-
-
     
 io.on('connection', function(socket){
-
+  
   socket.on('signal', function(msg){
-    io.emit('signal', msg);
-    console.log("signal \x1b[36m" + msg.signal + " (WEBSOCKET: " + socket.id + ")\x1b[0m"+" received with message: \x1b[36m" + msg.message + "\x1b[0m");
+    if (msg.signal) { 
+
+      if (msg.message) { var message = req.body.message; }
+      else { var message = null; }
+
+      io.emit('signal', msg); 
+      console.log(" signal \x1b[36m" + msg.signal + " (WEBSOCKET: " + socket.id + ")\x1b[0m"+" received with message: \x1b[36m" + message + "\x1b[0m");
+    }
+    else {
+      console.log("\x1b[35m WEBSOCKET - Missing 'signal' parameter (json)\x1b[0m");
+    }
+
     // socket.broadcast.emit('signal', msg);
   });
 
 
 });
 
-app.all("/", (req,res) => {
+
+
+
+app.get("/", (req,res) => {
 
   res.header('Access-Control-Allow-Origin', '*');
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/signal/:signal/:message', (req, res) => {
 
-  res.header('Access-Control-Allow-Origin', '*');
-  var change = {"signal": req.params.signal, "message": req.params.message};
 
-  io.emit('signal', change);
-  console.log("Signal \x1b[36m" + req.params.signal +" (HTTP GET) \x1b[0m received with message: \x1b[36m" + req.params.message + "\x1b[0m");
-  res.json(change);
+app.post("/", (req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+
+    if (req.body.message) { var message = req.body.message; }
+    else { var message = null; }
+
+    if (req.body.signal) {
+        var change = {"signal": req.body.signal, "message": message};
+        io.emit('signal', change);
+        console.log(" Signal \x1b[36m" + req.body.signal +" (HTTP POST)\x1b[0m received with message: \x1b[36m" + message + "\x1b[0m");
+        res.json(change);
+    }
+    else {
+      console.log("\x1b[35m HTTP POST - Missing 'signal' parameter (x-www-form-urlencoded)");
+      res.status(404).json({"error":"Missing 'signal' parameter (x-www-form-urlencoded)"});
+    }
 
 });
 
-
-app.get('/signal/:signal', (req, res) => {
-
-  res.header('Access-Control-Allow-Origin', '*');
-  var change = {"signal": req.params.signal, "message": "ok"};
-
-  io.emit('signal', change);
-  console.log("Signal \x1b[36m" + req.params.signal +" (HTTP GET) \x1b[0m received with message: \x1b[36m" + req.params.message + "\x1b[0m");
-  res.json(change);
-  
-});
